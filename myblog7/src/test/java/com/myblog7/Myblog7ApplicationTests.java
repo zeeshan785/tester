c98b9909714abbc804ad61390fef7bca06de7c20
alamzeeshan785@gmail.com <alamzeeshan785@gmail.com>
2025-09-24 18:57:15 +0530

    added

--- myblog7/src/test/java/com/myblog7/Myblog7ApplicationTests.java
+++ myblog7/src/test/java/com/myblog7/Myblog7ApplicationTests.java
@@ -9,11 +9,8 @@ class Myblog7ApplicationTests {
 	@Test
 	void contextLoads() {
 		char ch ='a';
-<<<<<<< HEAD
+                boolean isSub = true;
                 float f = 10.5;
-=======
-		boolean isSub = true;
->>>>>>> 03e65eef9bb7d695634cca35097fe4d295c49121
 	}
 
 }
